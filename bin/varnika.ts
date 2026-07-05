33251fd66ad31f0055d8d180bb0943bdc3b23566
Testuser <test@example.com>
2026-07-05 18:49:31 +0530

    update config file

--- bin/varnika.ts
+++ bin/varnika.ts
@@ -1,18 +1,31 @@
 #!/usr/bin/env node
 
 import * as cdk from 'aws-cdk-lib';
-
 import { VarnikaStack } from '../lib/varnika-stack';
+
+import devConfig from '../config/dev.json';
+import prodConfig from '../config/prod.json';
 
 const app = new cdk.App();
 
+const environment =
+  app.node.tryGetContext('env') || 'dev';
+
+const config =
+  environment === 'prod'
+    ? prodConfig
+    : devConfig;
+
 new VarnikaStack(
   app,
-  'VarnikaAppInfraStack',
+  `Varnika-${environment}`,
   {
     env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION
-    }
+      account: config.accountId,
+      region: config.region
+    },
+
+    terminationProtection:
+      environment === 'prod'
   }
 );
